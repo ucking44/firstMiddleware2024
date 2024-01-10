04a674b55273507fa011f78601014e91c84e3e76
ucking44 <ucking4niga@yahoo.com>
2024-01-10 07:53:29 +0100

    Initial Commit

---
+++ app/Models/Company.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    use HasFactory;
+
+    protected $table = "companies";
+
+    protected $primaryKey = "id";
+
+    protected $fillable = [
+        'company_name',
+        'status',
+    ];
+
+    public function program(){
+       return $this->hasMany(LoyaltyProgram::class);
+    }
+
+}
